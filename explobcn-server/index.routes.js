95fd87a40e72d292ded1a3903cb20af1c3225efc
Yabel <yabelrr@gmail.com>
2023-06-21 09:58:35 +0200

    Server project now functional

---
+++ explobcn-server/index.routes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const Itinerary = require("../models/Itinerary.model");
+const Location = require("../models/Location.model");
+
+router.get("/", (req, res, next) => {
+  res.json("All good in here");
+});
+
+router.get("/itinerary/:itineraryId", (req, res, next)=>{
+  let {itineraryId} = req.params;
+  Itinerary.findById(itineraryId)
+  .populate("attraction locationList")
+  .then((itinerary)=>{
+    res.json(itinerary)
+  })
+  .catch((err)=>next(err))
+}) 
+
+router.post("/createItinerary", (req, res, next) => {
+  let { attraction, locationList } = req.body;
+  const newItinerary = { attraction, locationList };
+  Itinerary.create(newItinerary)
+  .then((itinerary)=>{
+    res.json(itinerary)
+  })
+  .catch((err)=>next(err))
+})
+router.post("/createLocation", (req, res, next) => {
+  const {name, picture, description, popularity, coordinates} = req.body
+  Location.create({name, picture, description, popularity, coordinates})
+  .then(response => {
+    res.json({message: 'Location succesfully created!', location: response})
+  })
+  .catch(err => {next(err)})
+
+});
+
+
+module.exports = router;
